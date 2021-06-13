c3aaa802d39902e053bc1e6018cf53b844a77a65
taimamuang <84404957+taimamuang@users.noreply.github.com>
2021-06-13 23:55:11 +0900

    Update tscounter.c

--- tscounter.c
+++ tscounter.c
@@ -3,6 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+union semun {
+    int val;
+    struct semid_ds *buf;
+    ushort *array;
+};
+
+#define PATH "/home/jongchank/key"
 
 typedef struct __counter_t {
     int value;
@@ -20,15 +31,13 @@ void init(counter_t *c) {
 void increment(counter_t *c) {
     pthread_mutex_lock(&c->lock);
     c->value++;
-    if (c >= 0)
-            pthread_mutex_unlock(&c->lock);
+    pthread_mutex_unlock(&c->lock);
 }
 
 void decrement(counter_t *c) {
     pthread_mutex_lock(&c->lock);
     c->value--;
-    if (c < 0)
-            pthread_mutex_unlock(&c->lock);
+    pthread_mutex_unlock(&c->lock);
 }
 
 int get(counter_t *c) {
@@ -52,18 +61,48 @@ void *mythread(void *arg)
 }
                                                                              
 int main(int argc, char *argv[])
-{                    
+{             
+    key_t key;
+    int semid;
+    struct sembuf s;
+    union semun arg;
+    
+    key = ftok(PATH, 'z');
+    if (key < 0) {
+        perror(argv[0]);
+        exit(1);
+    }
+    semid = semget(key, 1, 0600 | IPC_CREAT);
+    if (semid < 0) {
+        perror(argv[0]);
+        exit(1);
+    }
+    
     loop_cnt = atoi(argv[1]);
 
     init(&counter);
 
     pthread_t p1, p2;
+    
+    s.sem_num = 0;
+    s.sem_op = -1; 
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    
     printf("main: begin [counter = %d]\n", get(&counter));
     pthread_create(&p1, NULL, mythread, "A"); 
     pthread_create(&p2, NULL, mythread, "B");
     // join waits for the threads to finish
     pthread_join(p1, NULL); 
     pthread_join(p2, NULL); 
+    
+    s.sem_num = 0;
+    s.sem_op = 1;
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    
+    semctl(semid, 0, IPC_RMID, NULL);  
+    
     printf("main: done [counter: %d] [should be: %d]\n", get(&counter), loop_cnt * 2);
     return 0;
 }
