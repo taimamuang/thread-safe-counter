a505657f176aa2c78c5f631bcb178ef7d0507004
taimamuang <84404957+taimamuang@users.noreply.github.com>
2021-06-14 00:00:13 +0900

    Update tscounter.c

--- tscounter.c
+++ tscounter.c
@@ -49,19 +49,6 @@ int get(counter_t *c) {
 
 void *mythread(void *arg)
 {
-    char *letter = arg;
-    int i;
-
-    printf("%s: begin\n", letter);
-    for (i = 0; i < loop_cnt; i++) {
-        increment(&counter);
-    }
-    printf("%s: done\n", letter);
-    return NULL;
-}
-                                                                             
-int main(int argc, char *argv[])
-{             
     key_t key;
     int semid;
     struct sembuf s;
@@ -72,11 +59,35 @@ int main(int argc, char *argv[])
         perror(argv[0]);
         exit(1);
     }
+    
     semid = semget(key, 1, 0600 | IPC_CREAT);
     if (semid < 0) {
         perror(argv[0]);
         exit(1);
     }
+    
+    char *letter = arg;
+    int i;
+
+    printf("%s: begin\n", letter);
+    s.sem_num = 0;
+    s.sem_op = -1; 
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    for (i = 0; i < loop_cnt; i++) {
+        increment(&counter);
+    }
+    s.sem_num = 0;
+    s.sem_op = 1;
+    s.sem_flg = 0;
+    semop(semid, &s, 1);
+    printf("%s: done\n", letter);
+    semctl(semid, 0, IPC_RMID, NULL);  
+    return NULL;
+}
+                                                                             
+int main(int argc, char *argv[])
+{ 
     
     loop_cnt = atoi(argv[1]);
 
@@ -84,24 +95,12 @@ int main(int argc, char *argv[])
 
     pthread_t p1, p2;
     
-    s.sem_num = 0;
-    s.sem_op = -1; 
-    s.sem_flg = 0;
-    semop(semid, &s, 1);
-    
     printf("main: begin [counter = %d]\n", get(&counter));
     pthread_create(&p1, NULL, mythread, "A"); 
     pthread_create(&p2, NULL, mythread, "B");
     // join waits for the threads to finish
     pthread_join(p1, NULL); 
     pthread_join(p2, NULL); 
-    
-    s.sem_num = 0;
-    s.sem_op = 1;
-    s.sem_flg = 0;
-    semop(semid, &s, 1);
-    
-    semctl(semid, 0, IPC_RMID, NULL);  
     
     printf("main: done [counter: %d] [should be: %d]\n", get(&counter), loop_cnt * 2);
     return 0;
